c4b4f17d6f02cb67b65b9121e36a5ebf0d49a077
hboppe <hanna.nascimento31@gmail.com>
2022-12-15 10:54:06 -0500

    Fazendo a funcao de renderizar jobs selecionados

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,6 +4,8 @@ function renderJobs(jobsList){
 
     const jobsSection = document.querySelector('.jobsList');
 
+    localStorage.setItem('@webwomen:jobs', []);
+
     jobsList.map((job) => {
         jobsSection.insertAdjacentHTML("beforeend", 
         `
@@ -27,12 +29,84 @@ function renderJobs(jobsList){
                 <p class="tag">${job.modalities[1]}</p>
                 </div>
               
-              <button class="applyButton" data-id: "${job.id}">Apply</button>
+              <button class="applyButton" data-id= "${job.id}" >Apply</button>
             </div>
         `)
     })
 }
 
-renderJobs(jobsData)
+renderJobs(jobsData);
 
+function selectJobs(jobsList) {
+    const addJobButtons = [...document.querySelectorAll('.applyButton')];
 
+    addJobButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            const selectedJob = jobsData.filter(job => job.id === Number(event.target.dataset.id));
+
+            const selectedJobsLS = JSON.stringify(localStorage.getItem('@webwomen:jobs'));
+
+            if(!selectedJobsLS.find(selectedJob)) {
+                selectedJobsLS.push(selectedJob);
+
+                localStorage.getItem('@webwomen:jobsselected') = JSON.stringify(selectedJobsLS);
+
+                renderSelectedJobs(selectedJobsLS);
+            }
+        })
+    })
+}
+
+selectJobs(jobsData)
+
+function renderSelectedJobs(jobsSelected){
+    const selectedJobsList = document.querySelector('.selectedJobsList');
+
+    const selectedJobsLS = JSON.parse(localStorage.getItem('@webwomen:jobs'));
+
+    selectedJobsList.innerHTML = '',
+
+    jobsSelected.forEach((job) => {
+        selectedJobsList.insertAdjacentHTML('beforeend', 
+        `
+        <li class="selectedJob">
+
+            <h5>${job.title}</h5>
+
+            <div class="small__container">
+
+              <small>${jb.enterprise}</small>
+
+              <small>${job.location}</small>
+
+            </div>
+
+            <button class="removeButton" data-id="${job.id}"><img src="/assets/img/trash.svg" alt="Trash icon"></button>
+        </li>
+        <hr>
+        `)
+
+    })
+
+}
+
+renderSelectedJobs(JSON.stringify(localStorage.getItem('@webwomen:jobsselected')))
+
+/* 
+
+<li class="selectedJob">
+
+            <h5>Pessoa desenvolvedora front-end - React JS</h5>
+
+            <div class="small__container">
+
+              <small>Kenzie Academy</small>
+
+              <small>Curitiba</small>
+
+            </div>
+
+            <button class="removeButton" data-id=""><img src="/assets/img/trash.svg" alt="Trash icon"></button>
+          </li>
+          <hr>
+*/
